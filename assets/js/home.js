6ce1ee876cb18c2ebab651e49ed315ee877eba52
David Daza <03dazal@gmail.com>
2020-06-30 16:16:02 -0500

    completa estructura de storytelling para desktop

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,46 +8,84 @@ const slider = new Swiper('.swiper-container', {
   },
 });
 
-const storytellingContainer = document.getElementById('storytelling');
-const graphics = document.querySelectorAll('.graphic');
-const lastGraphic = graphics[graphics.length - 1];
-const controller = new ScrollMagic.Controller();
-
-const scene = new ScrollMagic.Scene({
-  triggerHook: 0,
-  triggerElement: storytellingContainer,
-  duration:
-    storytellingContainer.getBoundingClientRect().height -
-    lastGraphic.getBoundingClientRect().height,
-})
-  .setPin('#steps')
-  .addTo(controller);
-
-scene.on('enter', function () {
-  graphics.forEach(function (graphic, index) {
-    const graphicScene = new ScrollMagic.Scene({
+// This is really specific to this website, so efforts to create a general purpose class are dismissed
+class Storytelling {
+  constructor(container, pinSelector) {
+    this.container = container;
+    this.pinSelector = pinSelector;
+    this.graphics = container.querySelectorAll('.graphic');
+    this.lastGraphic = this.graphics[this.graphics.length - 1];
+    this.controller = new ScrollMagic.Controller();
+  }
+  init() {
+    this.scene = new ScrollMagic.Scene({
       triggerHook: 0,
-      triggerElement: graphic,
-      duration: '100%',
-    }).addTo(controller);
+      triggerElement: this.container,
+      duration: this.getContainerHeight(),
+    })
+      .setPin(this.pinSelector)
+      .addTo(this.controller);
+    this.listen();
+  }
+  listen() {
+    this.scene.on('enter', () => {
+      this.graphics.forEach((graphic, index) => {
+        const graphicScene = new ScrollMagic.Scene({
+          triggerHook: 0.2,
+          triggerElement: graphic,
+          duration: '100%',
+        }).addTo(this.controller);
 
-    // onLeave
-    graphicScene.on('leave', function (event) {
-      const el = event.target.triggerElement();
-      if (el === lastGraphic && event.scrollDirection === 'FORWARD') {
-        return;
-      }
-      const step = document.getElementById(el.dataset.step);
-      step.classList.remove('opacity-100');
-      step.classList.add('opacity-0');
-    });
+        // onLeave
+        graphicScene.on('leave', (event) => {
+          const el = event.target.triggerElement();
+          if (el === this.lastGraphic && event.scrollDirection === 'FORWARD') {
+            return;
+          }
+          const step = document.getElementById(el.dataset.step);
+          step.classList.remove('opacity-100');
+          step.classList.add('opacity-0');
+        });
 
-    // onEnter
-    graphicScene.on('enter', function (event) {
-      const el = event.target.triggerElement();
-      const step = document.getElementById(el.dataset.step);
-      step.classList.remove('opacity-0');
-      step.classList.add('opacity-100');
+        // onEnter
+        graphicScene.on('enter', (event) => {
+          const el = event.target.triggerElement();
+          const step = document.getElementById(el.dataset.step);
+          step.classList.remove('opacity-0');
+          step.classList.add('opacity-100');
+        });
+      });
     });
-  });
-});
+  }
+  getContainerHeight() {
+    return (
+      this.container.getBoundingClientRect().height -
+      this.lastGraphic.getBoundingClientRect().height
+    );
+  }
+}
+
+const overviewStorytelling = new Storytelling(
+  document.getElementById('storytelling'),
+  '#steps'
+);
+
+const wayuuStorytelling = new Storytelling(
+  document.getElementById('storytelling-wayuu'),
+  '#steps-wayuu'
+);
+
+const gunaduleStorytelling = new Storytelling(
+  document.getElementById('storytelling-gunadule'),
+  '#steps-gunadule'
+);
+
+const tikunaStorytelling = new Storytelling(
+  document.getElementById('storytelling-tikuna'),
+  '#steps-tikuna'
+);
+
+overviewStorytelling.init();
+wayuuStorytelling.init();
+gunaduleStorytelling.init();
+tikunaStorytelling.init();
