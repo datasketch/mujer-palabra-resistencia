2cba0e8e1cf968f8f668346d278375d2e511ff74
David Daza <03dazal@gmail.com>
2020-07-03 10:33:44 -0500

    Habilita burger menu

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,15 @@
-window.addEventListener('DOMContentLoaded', function() {
+const menuTrigger = document.getElementById('menu-trigger');
+const navMenu = document.getElementById('nav-menu');
+const menuBarIcon = menuTrigger.querySelector('#menu-bars');
+const menuCloseIcon = menuTrigger.querySelector('#menu-close');
+
+menuTrigger.addEventListener('click', function () {
+  navMenu.classList.toggle('hidden');
+  navMenu.classList.toggle('flex');
+  menuBarIcon.classList.toggle('hidden');
+  menuCloseIcon.classList.toggle('hidden');
+});
+
+window.addEventListener('DOMContentLoaded', function () {
   document.body.classList.remove('pre');
-})
+});
